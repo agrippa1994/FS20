cfd64e5996c5abca1f4eaef2004c4cbe2c72848b
agrippa1994 <agrippa1994@icloud.com>
2015-09-24 20:17:31 +0200

    Added iOS Today Extension

---
+++ iOS/Today-Extension/TodayViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.swift
+//  Today-Extension
+//
+//  Created by Manuel Stampfl on 24.09.15.
+//  Copyright © 2015 Mani. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        
+        let a = NSUserDefaults.standardUserDefaults().valueForKey("Host") as? String
+        NSLog("\(a)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+
+}
