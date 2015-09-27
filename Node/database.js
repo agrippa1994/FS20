77d83372e9ea978a43899b65f1c4671a27aceb0d
agrippa1994 <agrippa1994@icloud.com>
2015-09-27 14:34:36 +0200

    Updated database

--- Node/database.js
+++ Node/database.js
@@ -3,11 +3,11 @@
 	
 	[{
 		name: "Test"
-		code1: 1111,
-		code2: 1111,
+		code1: 0xFF,
+		code2: 0x00,
 		devices: [{
 			name: "Test",
-			code: 1111,
+			code: 0xAA,
 			timeout: {
 				time: 120.0,
 				operation: true
@@ -25,18 +25,31 @@ var fs = require("fs");
 
 var data =[];
 
-function createRoom(name, code1, code2) {
-	for(var i = 0; i < data.length; i++)
-		if(data[i].name == name)
-			throw "Room already exists";
-		
-	data.push({ name: name, code1: code1, code2: code2, devices: [] });
-	save();
-}
- 
+// ============================================================================
+// Rooms
 function checkRoom(index) {
 	if(index >= data.length)
 		throw "No room at this index";
+}
+
+function checkRoomName(name) {
+	for(var i = 0; i < data.length; i++)
+		if(data[i].name == name)
+			throw "Room already exists";
+}
+
+function checkCode(code) {
+	if(code < 0x00 || code > 0xFF)
+		throw "Invalid FS20 code";
+}
+
+function createRoom(name, code1, code2) {
+	checkRoomName(name);
+	checkCode(code1);
+	checkCode(code2);
+	
+	data.push({ name: name, code1: code1, code2: code2, devices: [] });
+	save();
 }
 
 function getRooms() {
@@ -69,15 +82,12 @@ function deleteRoomAt(index) {
 	save();
 }
 
-function createDeviceAt(index, name, code) {
-	checkRoom(index);
-	
+// ============================================================================
+// Devices
+function checkDeviceName(index, name) {
 	for(var i = 0; i < data[index].devices.length; i++)
 		if(data[index].devices.name == name)
 			throw "Device already exists";
-	
-	data[index].devices.push({name: name, code: code, timeout: {}, timers: [] });
-	save();
 }
 
 function checkDevice(roomIndex, deviceIndex) {
@@ -85,6 +95,15 @@ function checkDevice(roomIndex, deviceIndex) {
 	
 	if(deviceIndex >= data[roomIndex].devices.length)
 		throw "Device doesn't exist";
+}
+
+function createDeviceAt(index, name, code) {
+	checkRoom(index);
+	checkDeviceName(name);
+	checkCode(code);
+	
+	data[index].devices.push({name: name, code: code, timeout: {}, timers: [] });
+	save();
 }
 
 function getDevices(roomIndex) {
@@ -101,10 +120,8 @@ function getDevice(roomIndex, deviceIndex) {
 
 function updateDeviceAt(roomIndex, deviceIndex, name, code) {
 	checkDevice(roomIndex, deviceIndex);
-	
-	for(var i = 0; i < data[roomIndex].devices.length; i++)
-		if(data[roomIndex].devices.name == name)
-			throw "Device with this name already exists";
+	checkDeviceName(roomIndex, name);
+	checkCode(code);
 			
 	data[roomIndex].device[deviceIndex].name = name || data[roomIndex].device[deviceIndex].name;
 	data[roomIndex].device[deviceIndex].code = code || data[roomIndex].device[deviceIndex].code;
@@ -118,11 +135,23 @@ function deleteDeviceAt(roomIndex, deviceIndex) {
 	save();
 }
 
+// ============================================================================
+// Timeouts
 function setDeviceTimeoutAt(roomIndex, deviceIndex, timeoutIn, operation) {
 	checkDevice(roomIndex, deviceIndex);
 	
 	data[roomIndex].device[deviceIndex].timeout = { time: timeoutIn, operation: operation };
 	save();
+}
+
+function getDeviceTimeoutAt(roomIndex, deviceIndex) {
+	checkDevice(roomIndex, deviceIndex);
+	
+	var timeout = data[roomIndex].device[deviceIndex].timeout;
+	if(!("time" in timeout && "operation" in timeout))
+		throw "No timeout is set";
+		
+	return copy(timeout);
 }
 
 function clearDeviceTimeoutAt(roomIndex, deviceIndex) {
@@ -141,9 +170,40 @@ function load() {
 		JSON.parse(fs.readFileSync("database.json"));
 	} catch(e) {
 		data = [];
+		
+		try {
+			save();
+		} catch(e) {
+			
+		}
 	}
 }
 
 function save() {
 	fs.writeFileSync("database.json", data);
+}
+
+load();
+
+module.exports = {
+	// Rooms
+	createRoom: createRoom,
+	checkRoom: checkRoom,
+	getRooms: getRooms,
+	getRoom: getRoom,
+	updateRoomAt: updateRoomAt,
+	deleteRoomAt: deleteRoomAt,
+	
+	// Devices
+	createDeviceAt: createDeviceAt,
+	checkDevice: checkDevice,
+	getDevices: getDevices,
+	getDevice: getDevice,
+	updateDeviceAt: updateDeviceAt,
+	deleteDeviceAt: deleteDeviceAt,
+	
+	// Timeouts
+	setDeviceTimeoutAt: setDeviceTimeoutAt,
+	getDeviceTimeoutAt: getDeviceTimeoutAt,
+	clearDeviceTimeoutAt: clearDeviceTimeoutAt
 }
