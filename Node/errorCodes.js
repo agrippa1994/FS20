45297abb2f89d3ce77c9fbf31f123d25976695aa
agrippa1994 <agrippa1994@icloud.com>
2015-10-08 20:54:51 +0200

    Updated error codes

--- Node/errorCodes.js
+++ Node/errorCodes.js
@@ -2,23 +2,34 @@ var errorCodes = {
 	DATABASE_TYPE_ERROR: 1000,
 	DATABASE_NO_ROOM_AT_INDEX: 1001,
 	DATABASE_ROOM_EXISTS: 1002,
-	DATABASE_INVALID_FS20_CODE: 1003,
-	DATABASE_DEVICE_EXISTS: 1004,
-	DATABASE_NO_DEVICE_AT_INDEX: 1005,
-	DATABASE_NO_TIMEOUT_SET: 1006
+	DATABASE_DEVICE_EXISTS: 1003,
+	DATABASE_NO_DEVICE_AT_INDEX: 1004,
+	DATABASE_NO_TIMEOUT_SET: 1005,
+	
+	INVALID_FS20_CODE: 1100,
 };
 
 var errorMessages = {
 	1000: "Type error",
 	1001: "No room at this index",
 	1002: "Room already exist",
-	1003: "Invalid FS20 code",
-	1004: "Device alreay exist",
-	1005: "No device at this index",
-	1006: "No timeout set for this device"	
+	1003: "Device alreay exist",
+	1004: "No device at this index",
+	1005: "No timeout set for this device",
+	
+	1100: "Invalid FS20 code"	
 };
+
+function createError(errorCode) {
+	return { code: errorCode, message: errorMessages[errorCode] || "" };
+}
+
+function throwError(errorCode) {
+	throw createError(errorCode);
+}
 
 module.exports = {
 	codes: errorCodes,
-	messages: errorMessages
+	messages: errorMessages,
+	throwError: throwError
 };
