f4d1802dfd57ca1f9b768e2407e169976407ee8e
agrippa1994 <agrippa1994@icloud.com>
2016-01-29 23:08:18 +0100

    Upload

--- iOS/FS20/MainTableViewController.swift
+++ iOS/FS20/MainTableViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 
-class MainTableViewController: UITableViewController, DeviceTableViewCellDelegate {
+class MainTableViewController: BaseFS20TableViewController {
     
-    // MARK: - Vars
-    private var fs20: FS20?
-    private var houses: [FS20.House] = []
-
     // MARK: - Storyboard actions
     @IBAction func onRefresh(sender: UIRefreshControl) {
         self.refreshWithAlertController(nil)
     }
     
+    // MARK: - BaseFS20TableViewController
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,25 +34,6 @@ class MainTableViewController: UITableViewController, DeviceTableViewCellDelegat
         let center = NSNotificationCenter.defaultCenter()
         center.removeObserver(self)
     }
-        
-    // MARK: - Table view data source
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.houses.count
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.houses[section].devices.count
-    }
-
-    // MARK: - Table view delegate
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("EntryCell", forIndexPath: indexPath) as! DeviceTableViewCell
-
-        cell.titleLabel.text = self.houses[indexPath.section].devices[indexPath.row].name
-        cell.delegate = self
-        
-        return cell
-    }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return true
@@ -70,78 +48,19 @@ class MainTableViewController: UITableViewController, DeviceTableViewCellDelegat
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return false
     }
-
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.houses[section].name
-    }
     
-    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.whiteColor()
+    override func invalidUrl() {
+        // Open Settings
+        UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 
     }
 
-    // MARK: - Device table view cell delegate
-    func deviceTableViewCellEnable(cell: DeviceTableViewCell) {
-        let path = self.tableView.indexPathForCell(cell)!
-        self.houses[path.section].devices[path.row].enable()
-    }
-    
-    func deviceTableViewCellDisable(cell: DeviceTableViewCell) {
-        let path = self.tableView.indexPathForCell(cell)!
-        self.houses[path.section].devices[path.row].disable()
-    }
-    
     // MARK: - Selectors
     func appDidBecomeActive() {
         self.validateAndApplySettings()
-    }
-    
-    // MARK: - Methods
-    func validateAndApplySettings() {
-        // Read server host from iOS Settings application
-        if let url = NSURL(string: Settings.host) where url.host != nil {
-            // Create FS20 object if it's not initialized otherwise set the url
-            if self.fs20 == nil {
-                self.fs20 = FS20(url: url)
-            } else {
-                self.fs20?.url = url
-            }
-            
-            // Refresh data
-            self.refreshWithAlertController(nil)
-            return
-        }
-        
-        // Open Settings
-        UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
-    }
-    
-    func refresh(completion: (Bool -> Void)?) {
-        if self.fs20 == nil {
-            completion?(false)
-            return
-        }
-        
-        self.refreshControl?.beginRefreshing()
-        self.fs20?.houses { houses in
-            dispatch_async(dispatch_get_main_queue()) {
-                defer {
-                    self.refreshControl?.endRefreshing()
-                }
-                
-                if houses == nil {
-                    completion?(false)
-                    return
-                }
-                
-                self.houses = houses!
-                self.tableView.reloadData()
-                completion?(true)
-            }
-        }
     }
     
     func refreshWithAlertController(completion: (Bool -> Void)?) {
@@ -157,6 +76,9 @@ class MainTableViewController: UITableViewController, DeviceTableViewCellDelegat
             let controller = UIAlertController(title: "ERROR".localized, message: "MAINTABLEVIEWCONTROLLER_REFRESH_ERROR".localized, preferredStyle: .Alert)
             controller.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
             self.presentViewController(controller, animated: true, completion: nil)
+        
         }
     }
+    
+    
 }
