69bd10bb658531cd3c290bedebf8abe46589536a
agrippa1994 <agrippa1994@icloud.com>
2016-07-28 16:53:02 +0200

    Added devices

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ var fs20 = new FS20(
 
 hap.init("hap-db");
 
-var light = {
+var FAKE_LIGHT = {
   powerOn: false,
   brightness: 100, // percentage
   
@@ -59,81 +59,50 @@ var light = {
   }
 };
 
+var light = new hap.Accessory('Light', hap.uuid.generate('hap-nodejs:accessories:light'));
+var fan = new hap.Accessory('Ventilator', hap.uuid.generate('hap-nodejs:accessories:fan'));
 
-
-
-
-
-
-
-// Generate a consistent UUID for our light Accessory that will remain the same even when
-// restarting our server. We use the `uuid.generate` helper function to create a deterministic
-// UUID based on an arbitrary "namespace" and the word "light".
-var lightUUID = hap.uuid.generate('hap-nodejs:accessories:light');
-
-// This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
-var light = new hap.Accessory('Light', lightUUID);
-
-// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-light.username = "1A:2B:3C:4D:5E:FF";
-light.pincode = "031-45-154";
-
-// set some basic properties (these values are arbitrary and setting them is optional)
 light
   .getService(hap.Service.AccessoryInformation)
   .setCharacteristic(hap.Characteristic.Manufacturer, "Oltica")
   .setCharacteristic(hap.Characteristic.Model, "Rev-1")
   .setCharacteristic(hap.Characteristic.SerialNumber, "A1S2NASF88EW");
 
-// listen for the "identify" event for this Accessory
+fan
+  .getService(hap.Service.AccessoryInformation)
+  .setCharacteristic(hap.Characteristic.Manufacturer, "Oltica")
+  .setCharacteristic(hap.Characteristic.Model, "Rev-1")
+  .setCharacteristic(hap.Characteristic.SerialNumber, "A1S2NASF88EX");
+
 light.on('identify', function(paired, callback) {
-  //FAKE_LIGHT.identify();
   callback(); // success
 });
 
-// Add the actual Lightbulb Service and listen for change events from iOS.
-// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-light
-  .addService(hap.Service.Lightbulb, "Fake Light") // services exposed to the user should have "names" like "Fake Light" for us
-  .getCharacteristic(hap.Characteristic.On)
-  .on('set', function(value, callback) {
-    //FAKE_LIGHT.setPowerOn(value);
-    callback(); // Our fake Light is synchronous - this value has been successfully set
-  });
-
-// We want to intercept requests for our current power state so we can query the hardware itself instead of
-// allowing HAP-NodeJS to return the cached Characteristic.value.
-light
-  .getService(hap.Service.Lightbulb)
-  .getCharacteristic(hap.Characteristic.On)
-  .on('get', function(callback) {
-    
-    // this event is emitted when you ask Siri directly whether your light is on or not. you might query
-    // the light hardware itself to find this out, then call the callback. But if you take longer than a
-    // few seconds to respond, Siri will give up.
-    
-    var err = null; // in case there were any problems
-    
-    if (FAKE_LIGHT.powerOn) {
-      console.log("Are we on? Yes.");
-      callback(err, true);
-    }
-    else {
-      console.log("Are we on? No.");
-      callback(err, false);
-    }
-  });
-
-// also add an "optional" Characteristic for Brightness
-light
-  .getService(hap.Service.Lightbulb)
-  .addCharacteristic(hap.Characteristic.Brightness)
-  .on('get', function(callback) {
-    callback(null, FAKE_LIGHT.brightness);
-  })
-  .on('set', function(value, callback) {
-    //FAKE_LIGHT.setBrightness(value);
-    callback();
+fan.on('identify', function(paired, callback) {
+  callback(); // success
 });
 
-light.publish({ port: 51826, username: light.username, pincode: light.pincode });
+light
+  .addService(hap.Service.Lightbulb, "Licht")
+  .getCharacteristic(hap.Characteristic.On)
+  .on('set', function(value, callback) {
+
+    logger.info("Device state: " + value);
+    fs20.setDeviceState(255, 0, 1, value, function() {
+      callback();
+    });
+});
+
+fan
+  .addService(hap.Service.Fan, "Ventilator")
+  .getCharacteristic(hap.Characteristic.On)
+  .on('set', function(value, callback) {
+
+    logger.info("Device state: " + value);
+    fs20.setDeviceState(255, 0, 2, value, function() {
+      callback();
+    });
+});
+
+light.publish({ port: 51826, username: "1A:2B:3C:4D:5E:FF", pincode: "031-45-154" });
+fan.publish({ port: 51827, username: "1A:2B:3C:4D:5F:FF", pincode: "031-45-155" });
