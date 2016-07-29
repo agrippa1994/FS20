704d6223b3a5edccbdd6aa0349d020e10cfaa7a2
agrippa1994 <agrippa1994@icloud.com>
2016-07-29 11:59:55 +0200

    Added new device

---
+++ devices/steckdose_accessory.js
@@ -0,0 +1,26 @@
+const hap = require("hap-nodejs");
+
+var device = new hap.Accessory('Steckdose', hap.uuid.generate('hap-nodejs:accessories:steckdose'));
+
+device
+  .getService(hap.Service.AccessoryInformation)
+  .setCharacteristic(hap.Characteristic.Manufacturer, "FS20")
+  .setCharacteristic(hap.Characteristic.Model, "Socket")
+  .setCharacteristic(hap.Characteristic.SerialNumber, "None");
+
+device.on('identify', function (paired, callback) {
+  callback();
+});
+
+device
+  .addService(hap.Service.Fan, "Steckdose")
+  .getCharacteristic(hap.Characteristic.On)
+  .on('set', function (value, callback) {
+    device.stateChange(value, callback);
+  });
+
+device.username = "00:00:00:00:00:03";
+device.pincode = "000-00-003";
+device.code = 0xFFFF03;
+
+exports.accessory = device;
